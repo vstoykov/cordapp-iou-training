66d0742d514d8eae92e8cbda19b9d05f96c3beea
Venelin Stoykov <venelin.stoykov@industria.tech>
2018-06-11 15:01:44 +0300

    Add IOU State

--- cordapp/src/main/kotlin/tech/industria/training/iou/state/IOUState.kt
+++ cordapp/src/main/kotlin/tech/industria/training/iou/state/IOUState.kt
@@ -1,9 +1,17 @@
 package tech.industria.training.iou.state
 
-import net.corda.core.contracts.ContractState
-import net.corda.core.identity.AbstractParty
+import net.corda.core.contracts.Amount
+import net.corda.core.contracts.LinearState
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.Party
+import java.util.*
 
-
-data class IOUState(val data: String) : ContractState {
-    override val participants: List<AbstractParty> get() = listOf()
+data class IOUState(
+        val amount: Amount<Currency>,
+        val lender: Party,
+        val borrower: Party,
+        val paid: Amount<Currency> = Amount(0, amount.token),
+        override val linearId: UniqueIdentifier = UniqueIdentifier()
+) : LinearState {
+    override val participants get() = listOf(lender, borrower)
 }
